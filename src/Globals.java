0901f757a89917e72cca16d3766bfe10b2aa397a
Kevin Zhou <53985119+KevinZZhou@users.noreply.github.com>
2020-12-27 04:09:53 -0500

    Created Globals

---
+++ src/Globals.java
@@ -0,0 +1,28 @@
+// Globals creates various constants that are used in other files
+public interface Globals {
+	public static final int FIELD_X = 800;
+	public static final int FIELD_Y = 800;
+	
+	public static final int ALIEN_X = 40;
+	public static final int ALIEN_Y = 40;
+	public static final int ALIEN_VEL_X = 10;
+	public static final int ALIEN_VEL_Y = 0;
+	public static final int BOMB_X = 20;
+	public static final int BOMB_Y = 20;
+	public static final int BOMB_VEL_X = 0;
+	public static final int BOMB_VEL_Y = 20;
+	
+	public static final int SHIP_X = 50;
+	public static final int SHIP_Y = 100;
+	public static final int SHIP_VEL_X = 10;
+	public static final int SHIP_VEL_Y = 0;
+	public static final int SHOT_X = 10;
+	public static final int SHOT_Y = 50;
+	public static final int SHOT_VEL_X = 0;
+	public static final int SHOT_VEL_Y = -10;
+	
+	public static final int ASTEROID_X = 30;
+	public static final int ASTEROID_Y = 30;
+	public static final int ASTEROID_VEL_X = 0;
+	public static final int ASTEROID_VEL_Y = 3;
+}
